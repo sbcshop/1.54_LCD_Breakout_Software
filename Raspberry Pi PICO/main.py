d2acff818c395fc57f42424f51e275543eea3715
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-02-24 17:43:27 +0530

    Update main.py

--- Raspberry Pi PICO/main.py
+++ Raspberry Pi PICO/main.py
@@ -1,11 +1,8 @@
-# attendence system using raspberry pi pico and pico barcode hat and security system using barcode
 from machine import Pin, UART,SPI
 import time
 import st7789 #library of TFT display controller uses SPI interface
 import vga1_bold_16x32 as font
 
-
-# this create the data.txt file inside the raspberry pi pico
 
 spi = SPI(1, baudrate=40000000, sck=Pin(10), mosi=Pin(11))
 tft = st7789.ST7789(spi,240,240,reset=Pin(12, Pin.OUT),cs=Pin(9, Pin.OUT),dc=Pin(8, Pin.OUT),backlight=Pin(13, Pin.OUT),rotation=0)#SPI interface for tft screen
