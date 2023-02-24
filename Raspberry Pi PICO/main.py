b520c68f90b0d12a7259308c1fea12627de4ae3d
sbshop1 <96284682+sbcshop1@users.noreply.github.com>
2023-02-24 17:25:22 +0530

    Update main.py

--- Raspberry Pi PICO/main.py
+++ Raspberry Pi PICO/main.py
@@ -8,7 +8,7 @@ import vga1_bold_16x32 as font
 # this create the data.txt file inside the raspberry pi pico
 
 spi = SPI(1, baudrate=40000000, sck=Pin(10), mosi=Pin(11))
-tft = st7789.ST7789(spi,135,240,reset=Pin(12, Pin.OUT),cs=Pin(9, Pin.OUT),dc=Pin(8, Pin.OUT),backlight=Pin(13, Pin.OUT),rotation=3)#SPI interface for tft screen
+tft = st7789.ST7789(spi,240,240,reset=Pin(12, Pin.OUT),cs=Pin(9, Pin.OUT),dc=Pin(8, Pin.OUT),backlight=Pin(13, Pin.OUT),rotation=3)#SPI interface for tft screen
 
 def main():
     tft.init()
